cf100d7558011e5d4ddd8c163e62cdb37bd42afd
Lucas Kern <lucasgilbertokern@gmail.com>
2018-06-08 05:00:09 -0300

    Added Diffs per Spec as well.

--- ClassChangeParser/classChangeParser.py
+++ ClassChangeParser/classChangeParser.py
@@ -5,9 +5,11 @@ import re
 
 toc = '[toc]'
 separator = '[/toggler][/div][/toggler]'
+specSeparator = '[/toggler]\n[toggler class="heading-size-3'
 directory = 'output'
 tail = '[/toggler][/div]'
 baseHeader = '[h2][color={1}]{0} Spell Changes[/color][/h2][div class="patch-diff patch-diff-group-{2} patch-diff-class patch-diff-class-{3}"]'
+specHeader = '[h2]{0} {1} Changes[/h2]'
 nameRe = re.compile('(name=").*?(")')
 colorRe = re.compile('(class=").*?(")')
 
@@ -55,10 +57,28 @@ if len(sys.argv) > 1:
 				clsName = nameRe.search(firstLine).group(0)[6:-1]
 				color = colorRe.search(firstLine).group(0)[7:-1]
 				header = baseHeader.format(clsName, color, '-'.join( clsName.split(' ') ).lower(), color[1:] )
+				cleanContent = '\n'.join(content.split('\n')[1:])
+				for spec in cleanContent.split(specSeparator):
+					
+					firstSpecLine = spec.strip().split('\n')[0]
+					specName = 'Weapon Damage to AP Conversion'
+					start = 18
+					if firstSpecLine is not None and 'table' not in firstSpecLine and 'Azerite' not in firstSpecLine:						
+						specName = nameRe.search(firstSpecLine).group(0)[6:-1]
+						start = 1
+					cleanSpecContent = '\n'.join(spec.strip().split('\n')[start:])
+
+					with open('{0} {1}.txt'.format(clsName, specName), 'w') as f:
+						f.seek(0)
+						f.write(specHeader.format(specName, clsName))
+						f.write('\n')
+						f.write(cleanSpecContent)
+						f.truncate()
+
 
 				with open('{0}.txt'.format(clsName), 'w') as f:
 					f.seek(0)
 					f.write(header)
-					f.write('\n'.join(content.split('\n')[1:]))
+					f.write(cleanContent)
 					f.write(tail)
 					f.truncate()
